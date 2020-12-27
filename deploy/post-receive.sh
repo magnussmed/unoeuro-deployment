5d815961d650639af87623ff9570e153de26dce7
Magnus Smed <magnussmed1@gmail.com>
2020-12-27 18:52:33 +0100

    fix placeholders and fix errors + add stage deploy

--- deploy/post-receive.sh
+++ deploy/post-receive.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
-TARGET="/var/www/websider.dk/public_html"
-ROOT="/var/www/websider.dk"
-GIT_DIR="/var/www/websider.dk/repo/unoeuro-deployment.git"
-BRANCH="master"
+TARGET_PROD="/var/www/SIMPLY_SITE_DOMAIN/public_html"
+TARGET_STAGE="/var/www/SIMPLY_SITE_DOMAIN/staging"
+ROOT="/var/www/SIMPLY_SITE_DOMAIN"
+GIT_DIR="/var/www/SIMPLY_SITE_DOMAIN/repo/YOUR_REPO_NAME.git"
 
 while read oldrev newrev ref
 do
-	# Only checking out the master
-	if [ "$ref" = "refs/heads/$BRANCH" ];
+
+	BRANCH=`echo $ref | cut -d/ -f3`
+
+	# Deploy to production servers (only master branch allowed)
+	if [ "master" == "$BRANCH" ];
 	then
 		echo "Ref $ref received. Deploying ${BRANCH} branch to production..."
-		git --work-tree=$TARGET --git-dir=$GIT_DIR checkout -f $BRANCH
-		chmod +rx $TARGET/deploy/deploy-setup.sh
-		cd $TARGET/deploy
+		git --work-tree=$TARGET_PROD --git-dir=$GIT_DIR checkout -f $BRANCH
+		chmod +rx $TARGET_PROD/deploy/deploy-setup.sh
+		cd $TARGET_PROD/deploy
 		./deploy-setup.sh
 	else
-		echo "Ref $ref received. Doing nothing: only the ${BRANCH} branch may be deployed on this server."
+		# Deploy to stage...
+		echo "Ref $ref received. Deploying ${BRANCH} to stage..."
+		git --work-tree=$TARGET_STAGE --git-dir=$GIT_DIR checkout -f $BRANCH
+		chmod +rx $TARGET_STAGE/deploy/deploy-setup.sh
+		cd $TARGET_STAGE/deploy
+		./deploy-setup.sh
 	fi
 done
