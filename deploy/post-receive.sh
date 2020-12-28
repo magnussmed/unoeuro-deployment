91d113c4971e085704d6969559af52d7857c7028
Magnus Smed <magnussmed1@gmail.com>
2020-12-28 16:55:52 +0100

    add php environment variable to root .htaccess

--- deploy/post-receive.sh
+++ deploy/post-receive.sh
@@ -15,14 +15,18 @@ do
 		echo "Ref $ref received. Deploying ${BRANCH} branch to production..."
 		git --work-tree=$TARGET_PROD --git-dir=$GIT_DIR checkout -f $BRANCH
 		chmod +rx $TARGET_PROD/deploy/deploy-setup.sh
-		cd $TARGET_PROD/deploy
+		cd $TARGET_PROD
+		sed -i -e '$aSetEnv PHP_ENV production' .htaccess
+		cd deploy
 		./deploy-setup.sh
 	else
 		# Deploy to stage...
 		echo "Ref $ref received. Deploying ${BRANCH} to stage..."
 		git --work-tree=$TARGET_STAGE --git-dir=$GIT_DIR checkout -f $BRANCH
 		chmod +rx $TARGET_STAGE/deploy/deploy-setup.sh
-		cd $TARGET_STAGE/deploy
+		cd $TARGET_STAGE
+		sed -i -e '$aSetEnv PHP_ENV staging' .htaccess
+		cd deploy
 		./deploy-setup.sh
 	fi
 done
